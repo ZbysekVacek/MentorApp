e3d05ac58a643732f6a175ac1daa5472709c07c7
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-10-26 21:08:37 +0200

    Serve index and static files on BE dev server
    Related links:
    https://docs.djangoproject.com/en/4.2/howto/static-files/
    https://create-react-app.dev/docs/deployment/#building-for-relative-paths

--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'mentorapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'fe/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/build/"),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- mentorapp/urls.py
+++ mentorapp/urls.py
@@ -19,10 +19,15 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import path, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 from backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/meetings/$', views.meetings_list),
     re_path(r'^api/meetings/([0-9])$', views.meetings_detail),
-]
+    path('', TemplateView.as_view(template_name='index.html')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
