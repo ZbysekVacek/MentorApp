f5fbb4c47c2c12265c6050826d18747d8da7f410
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-10-28 17:35:23 +0200

    Load properties from env

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 * Go to the root folder
 * Create a virtual environment
 * Run `python -m venv mentorapp_env`
+* Run `pip install`
 ## Development
+### Required environment properties
+
+| Property                | Description                                                                   |
+|-------------------------|-------------------------------------------------------------------------------|
+| DJANGO_SECRET_KEY       | Django secret key property                                                    |
+| DJANGO_ALLOWED_HOSTS    | Which addresses or domain names may be used to connect to the Django instance |
+| DJANGO_DEBUG            | Sets the debug mode                                                           |
+| DJANGO_DEVELOPMENT_MODE | Toggles development mode and connection to SQLite / Postgres DB               |
+| DJANGO_DATABASE_URL     | URL for DB connection                                                         |
+
 ### How to run BE dev server
 * *Optional:* Activate your virtual env if you don't have it active yet (`source ./mentorapp_env/bin/activate`)
 * Go to the root folder
--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
+import dj_database_url
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+DEVELOPMENT_MODE = os.getenv("DJANGO_DEVELOPMENT_MODE", "False") == "True"
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k9=x53w_f-di03oa#va91fyd02!$+e6$6&u^+w^6#s@*5bhcj#'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -80,12 +85,20 @@ WSGI_APPLICATION = 'mentorapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DJANGO_DATABASE_URL", None) is None:
+        raise Exception("DJANGO_DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DJANGO_DATABASE_URL")),
+    }
+
 
 
 # Password validation
