4ebb915b8a7e5bb0375b635eac7930e524072616
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-11-03 12:14:39 +0100

    Test CDN

--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -140,6 +140,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/build"),
+    os.path.join(BASE_DIR, "frontend/test"),
 ]
 
 # Default primary key field type
