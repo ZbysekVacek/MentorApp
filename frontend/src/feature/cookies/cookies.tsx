516fdd39ff50986378c7617d880069445d42942d
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-11-03 14:21:28 +0100

    Handle situation when there are no cookies defined

--- frontend/src/feature/cookies/cookies.tsx
+++ frontend/src/feature/cookies/cookies.tsx
@@ -3,7 +3,9 @@ const parseCookie = (str: string) =>
     .split(';')
     .map((v) => v.split('='))
     .reduce((acc: Record<string, string>, v) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim())
+      if (v.length === 2) {
+        acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim())
+      }
       return acc
     }, {})
 
