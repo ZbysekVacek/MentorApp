3dd5236a1fbe8d5a4dc4f8f8f5cadb602e0acf44
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-11-08 11:24:44 +0100

    Add default exception handler

--- mentorapp/settings.py
+++ mentorapp/settings.py
@@ -147,3 +147,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "backend.features.exception.mentorapp_exception_handler.mentorapp_exception_handler"
+}
