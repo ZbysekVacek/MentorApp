32ce1f1df51cb3e565d1f6802155fe28405f66a3
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-11-24 17:41:18 +0100

    Add todo for homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Typography } from 'antd'
 import UserInfo from '../feature/user/UserInfo'
-import { NavLink } from 'react-router-dom'
 import RestrictedRoute from '../feature/routing/RestrictedRoute'
 import { useDocumentTitle } from '@uidotdev/usehooks'
 import { getRouteTitle } from '../feature/routing/routeDocumentTitle'
 import { Routes } from '../feature/routing/routes'
+import { useUserRetrieve } from '../api/generated/generatedApiComponents'
 
+// TODO MentorApp: implement the page
 const HomePage: React.FC = () => {
   useDocumentTitle(getRouteTitle(Routes.HomePage))
+  const { data: user } = useUserRetrieve({})
+
+  let userFullName =
+    user && user.first_name && user.last_name
+      ? ` ${user.first_name} ${user.last_name}`
+      : ''
 
   return (
     <RestrictedRoute>
-      <Typography.Title>Mentoring App</Typography.Title>
-      <UserInfo />
-      <NavLink to="/meetings">Meetings</NavLink>
+      <Typography.Title>Welcome back {userFullName}</Typography.Title>
     </RestrictedRoute>
   )
 }
