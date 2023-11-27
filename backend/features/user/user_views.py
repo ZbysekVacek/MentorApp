b69fe8568e1dee26ac150b8ac186836093a117a5
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-11-27 16:22:34 +0100

    Add user profile model

--- backend/features/user/user_views.py
+++ backend/features/user/user_views.py
@@ -4,8 +4,6 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework import generics, status
 from rest_framework.views import APIView
-from rest_framework.schemas.openapi import AutoSchema
-
 from backend.features.exception.exception_serializer import ExceptionSerializer
 from backend.features.user.user_serializers import (
     UserSerializer,
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -10,3 +11,20 @@ class Meeting(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class Profile(models.Model):
+    """Profile model is used to store additional information about the user"""
+
+    #  Link to the user model
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE, related_name="profile"
+    )
+    # Show user as a mentor who accepts mentees
+    accepts_mentees = models.BooleanField(blank=False, default=False)
+    # General information the  user wants to share about themselves
+    about = models.TextField(blank=True, null=True)
+    # Contact information the user wants to share
+    contact = models.TextField(blank=True, null=True)
+    # In what skills can user share knowledge
+    skills = models.TextField(blank=True, null=True)
