c54f928ad4e3208aae6c64cf5595d8dbdf14e211
Zbysek Vacek <zbysek.vacek@gmail.com>
2023-11-29 15:33:59 +0100

    Add competencies

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Meeting, Profile, Notification
+from .models import Meeting, Profile, Notification, Competency
 
 # Register your models here.
 admin.site.register(Meeting)
 admin.site.register(Profile)
 admin.site.register(Notification)
+admin.site.register(Competency)
